253bf7b37c1a1dbae2a49ba60cdac9a906ec5afd
chanjongpak <chanjong.pak@gmail.com>
2023-06-06 17:36:24 -0400

    add Button component

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -2,6 +2,7 @@
 
 import { useCallback, useEffect, useState } from "react";
 import { IoMdClose } from "react-icons/io";
+import Button from "../Button";
 
 interface ModalProps {
   isOpen?: boolean;
@@ -135,6 +136,7 @@ const Modal: React.FC<ModalProps> = ({
                 "
               >
                 <button
+                  onClick={handleClose}
                   className="
                     p-1
                     border-0
@@ -146,6 +148,22 @@ const Modal: React.FC<ModalProps> = ({
                 >
                   <IoMdClose size={18} />
                 </button>
+                <div className="text-lg font-semibold">{title}</div>
+              </div>
+              {/* Body */}
+              <div className="relative p-6 flex-auto">{body}</div>
+              {/* Footer */}
+              <div className="flex flex-col gap-2 p-6">
+                <div
+                  className="
+                flex
+                flex-row
+                items-center
+                gap-4
+                w-full"
+                >
+                  <Button />
+                </div>
               </div>
             </div>
           </div>
