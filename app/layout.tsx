f184fc66751ce5288db66e16d0dec2d09353a546
chanjongpak <chanjong.pak@gmail.com>
2023-06-06 16:59:41 -0400

    create Modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import { Nunito } from "next/font/google";
 import Navbar from "./components/navbar/Navbar";
 import ClientOnly from "./components/ClientOnly";
+import Modal from "./components/modals/Modal";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,6 +25,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
+          <Modal />
           <Navbar />
         </ClientOnly>
         {children}
